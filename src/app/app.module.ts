8d37f5bcce306952435a11ce22de23423ee87472
jack.lu <jack.lu@innovapost.com>
2024-03-06 15:20:02 -0500

    minor change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ NodeService ]
-
 })
 
 export class AppModule { }
