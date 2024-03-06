ab0784aaeaac2d8618ef627cbe78addf64ddf97b
jack.lu <jack.lu@innovapost.com>
2024-03-06 16:52:11 -0500

    works after local sharedModule

--- src/app/components/tree/tree.module.ts
+++ src/app/components/tree/tree.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { SharedModule } from "primeng/api";
+
 import { RippleModule } from "primeng/ripple";
 import { ScrollerModule } from "primeng/scroller";
 import { Tree } from "./tree";
 import { UITreeNode } from "./tree-node.component";
+import { SharedModule } from "../api/shared";
  
 
 @NgModule({
