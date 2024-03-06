adac5f18976ff312b8c1a4c0b6eb13ef021c815d
jack.lu <jack.lu@innovapost.com>
2024-03-06 13:54:54 -0500

    works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent }   from './app.component';
 
-//import { TreeSelectModule } from 'primeng/treeselect';
+import { TreeSelectModule } from 'primeng/treeselect';
 import {ButtonModule} from 'primeng/button';
 import {PanelModule} from 'primeng/panel';
 import {FormsModule} from '@angular/forms';
-import { NodeService } from './nodeservice';
+ 
 import { HttpClientModule } from '@angular/common/http';
-import { TreeSelectModule } from './treeselect/treeselect.module';
+import { NodeService } from './nodeservice';
+ 
  
 
 
