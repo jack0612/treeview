cb2c3045bfcc9342ad85fb7ad8033f15dd08338d
jack.lu <jack.lu@innovapost.com>
2024-03-06 08:57:46 -0500

    works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
+import { NodeService } from './nodeservice';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [`
+      :host ::ng-deep .p-treeselect {
+          width:20rem;
+          display: inline-flex;
+      }
+  `],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'treeselect';
+export class AppComponent { 
+    nodes1!: any[];
+
+    nodes2!: any[];
+
+    nodes3!: any[];
+
+    selectedNodes1: any[] = [];
+
+    selectedNodes2: any[] = [];
+
+    selectedNode: any;
+
+    constructor(public nodeService: NodeService) { }
+
+    ngOnInit() {
+        this.nodeService.getFiles().then(files => this.nodes1 = files);
+        this.nodeService.getFiles().then(files => this.nodes2 = files);
+        this.nodeService.getFiles().then(files => this.nodes3 = files);
+    }    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent }   from './app.component';
 
-import { AppComponent } from './app.component';
+import { TreeSelectModule } from 'primeng/treeselect';
+import {ButtonModule} from 'primeng/button';
+import {PanelModule} from 'primeng/panel';
+import {FormsModule} from '@angular/forms';
+import { NodeService } from './nodeservice';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    PanelModule,
+		FormsModule,
+    HttpClientModule,
+    TreeSelectModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ NodeService ]
+
 })
+
 export class AppModule { }
